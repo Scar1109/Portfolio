674db22ec0805315f4d3597e0a49d75ad197da3d
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-04-15 03:28:37 +0530

    PR | Heroku update

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.use(express.json());
 
 app.use("/api/emails", emailRoute);
 
+if (process.env.NODE_ENV === "production") {
+    // Set the static folder
+    app.use(express.static("client/dist"));
+
+    // Serve the index.html file for all non-API routes
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
