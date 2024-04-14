8f3019a764d29e2b95f3a5b09fef111cdc729be8
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-04-14 14:02:04 +0530

    PF | Legacy peer heroku

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
-app.use(express.json());
+const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client", "build")));
+    // Set the static folder
+    app.use(express.static("client/dist"));
+
+    // Serve the index.html file for all non-API routes
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
     });
 }
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log("Node Server Started using Nodemon!"));
