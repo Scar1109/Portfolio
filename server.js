afad80dd86f7aa1d1a7a48f11e0e30dbe0df2e9c
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-04-14 13:19:45 +0530

    EF | Heroku update

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client", "build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
